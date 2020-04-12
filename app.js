d6427843ed9b3ef89d0e6d3c8c58d2f22dca2a09
alejo4373 <alejo4373@gmail.com>
2020-04-12 17:49:48 -0400

    integrate autoprefixer with postcss middleware

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware')
+var postcssMiddleware = require('postcss-middleware')
+var autoprefixer = require('autoprefixer')
 
 var app = express();
 
 app.set('view engine', 'pug')
-console.log(path.join(__dirname, 'public/stylesheets'))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +18,12 @@ app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
 }))
+
+app.use('/stylesheets', postcssMiddleware({
+  plugins: [autoprefixer()],
+  src: (req) => path.join(__dirname, 'public/stylesheets', req.path)
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/ilearned', (req, res, next) => {
