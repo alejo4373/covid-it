2c50fd7dc8173761090489da08e40ea9e580f5d3
alejo4373 <alejo4373@gmail.com>
2020-04-12 16:22:31 -0400

    set up sass preprocessor

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var sassMiddleware = require('node-sass-middleware')
 
 var app = express();
 
 app.set('view engine', 'pug')
+console.log(path.join(__dirname, 'public/stylesheets'))
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/ilearned', (req, res, next) => {
