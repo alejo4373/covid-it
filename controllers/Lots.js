88fa1148c1fd781011a334eb35265b20a90f0f8f
alejo4373 <alejandrofranco@pursuit.org>
2020-04-17 02:03:33 -0400

    remove console.log

--- controllers/Lots.js
+++ controllers/Lots.js
@@ -2,7 +2,6 @@ const { ParkingLot } = require('../db/models')
 
 const renderIndex = async (req, res, next) => {
   const lots = await ParkingLot.getAll()
-  console.log(lots)
   res.render('index', { lots })
 }
 
