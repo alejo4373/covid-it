37e8c1d7733ce984c0ec01e56d90f2c6451a6833
alejo4373 <alejandrofranco@pursuit.org>
2020-04-21 13:18:03 -0400

    fix no catory title showing in notes category pages

--- controllers/web/notes.js
+++ controllers/web/notes.js
@@ -3,8 +3,21 @@ const { Notes } = require('../../db/models')
 const renderNotes = async (req, res, next) => {
   const { category } = req.params
 
+  let formattedTitle = ""
+  switch (category) {
+    case "learned":
+      formattedTitle = "We Have Learned";
+      break;
+    case "wanttodo":
+      formattedTitle = "We Want to Do"
+      break;
+    case "gratefulfor":
+      formattedTitle = "We Are Grateful For"
+      break;
+  }
+
   const notes = await Notes.getAllByCategory(category)
-  res.render('notes', { notes })
+  res.render('notes', { notes, title: formattedTitle })
 }
 
 const renderIndex = (req, res, next) => {
